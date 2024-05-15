e0ed447e04d134305dab9db2e186683473fe229d
Akhmad Sugiannoor <AkhmadSugiannoor9@gmail.com>
2024-05-15 22:08:20 +0800

    Dashboard

--- src/features/dashboard/api/dashboard.ts
+++ src/features/dashboard/api/dashboard.ts
@@ -4,3 +4,7 @@ export const getChart = async () => {
     const response = await axios.get("/dashboard/chart");
     return response.data.data;
   };
+export const getDashboard = async () => {
+    const response = await axios.get("/dashboard/data");
+    return response.data.data;
+  };
--- src/features/dashboard/components/CardDashboard.tsx
+++ src/features/dashboard/components/CardDashboard.tsx
@@ -1,12 +1,18 @@
 import { Card, CardBody, CardFooter, Typography } from "@material-tailwind/react"
+import { useQuery } from "react-query";
+import { getDashboard } from "../api/dashboard";
 
 const CardDashboard = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["dashboard"],
+    queryFn: getDashboard,
+  });
   return (
     <div>
       <Card placeholder={""} className="mt-6 w-[100%]">
       <CardBody placeholder={""} className="flex flex-col lg:flex-row justify-center items-center">
         <div className= "grid place-content-center rounded-full bg-white w-[90px] h-[90px] border-8 border-[#005697]">
-            <div className="font-poppins text-xl font-semibold text-black">125</div>
+            <div className="font-poppins text-xl font-semibold text-black">{data.total_users}</div>
         </div>
         <div className="items-center">
         <Typography placeholder={""} className="ml-5 text-md mt-5 lg:mt-0 lg:text-xl font-semibold font-poppins">
