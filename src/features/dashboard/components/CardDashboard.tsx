3997c562a6255ee3d0097d067ca754dc5e84e32b
Akhmad Sugiannoor <AkhmadSugiannoor9@gmail.com>
2024-05-16 12:20:06 +0800

    Dashboard

--- src/features/dashboard/components/CardDashboard.tsx
+++ src/features/dashboard/components/CardDashboard.tsx
@@ -1,25 +1,51 @@
 import { Card, CardBody, CardFooter, Typography } from "@material-tailwind/react"
 import { useQuery } from "react-query";
 import { getDashboard } from "../api/dashboard";
-
+type total = {
+  total_users: number;
+  total_contracts: number;
+  total_complaints: number;
+}
 const CardDashboard = () => {
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery<total>({
     queryKey: ["dashboard"],
     queryFn: getDashboard,
   });
+
   return (
     <div>
       <Card placeholder={""} className="mt-6 w-[100%]">
       <CardBody placeholder={""} className="flex flex-col lg:flex-row justify-center items-center">
         <div className= "grid place-content-center rounded-full bg-white w-[90px] h-[90px] border-8 border-[#005697]">
-            <div className="font-poppins text-xl font-semibold text-black">{data.total_users}</div>
+            <div className="font-poppins text-xl font-semibold text-black">{data?.total_users ?? "-"}</div>
         </div>
         <div className="items-center">
         <Typography placeholder={""} className="ml-5 text-md mt-5 lg:mt-0 lg:text-xl font-semibold font-poppins">
           Total Pengguna
         </Typography>
-        <Typography  placeholder={""} className="ml-5 text-sm lg:text-xl font-semibold font-poppins">
-          22
+        </div>
+      </CardBody>
+    </Card>
+      <Card placeholder={""} className="mt-6 w-[100%]">
+      <CardBody placeholder={""} className="flex flex-col lg:flex-row justify-center items-center">
+        <div className= "grid place-content-center rounded-full bg-white w-[90px] h-[90px] border-8 border-[#005697]">
+            <div className="font-poppins text-xl font-semibold text-black">{data?.total_contracts ?? "-"}</div>
+        </div>
+        <div className="items-center">
+        <Typography placeholder={""} className="ml-5 text-md mt-5 lg:mt-0 lg:text-xl font-semibold font-poppins">
+          Total Kontrak
+        </Typography>
+        </div>
+      </CardBody>
+    </Card>
+      <Card placeholder={""} className="mt-6 w-[100%]">
+      <CardBody placeholder={""} className="flex flex-col lg:flex-row justify-center items-center">
+        <div className= "grid place-content-center rounded-full bg-white w-[90px] h-[90px] border-8 border-[#005697]">
+            <div className="font-poppins text-xl font-semibold text-black">{data?.total_complaints ?? "-"}</div>
+        </div>
+        <div className="items-center">
+        <Typography placeholder={""} className="ml-5 text-md mt-5 lg:mt-0 lg:text-xl font-semibold font-poppins">
+         Total Komplaint
         </Typography>
         </div>
       </CardBody>
