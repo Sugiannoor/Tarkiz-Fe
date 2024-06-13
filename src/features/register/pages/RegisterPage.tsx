94883be27af8bef0828692a8ef4b30c7406979eb
Sugiannoor <akhmadsugiannoor9@gmail.com>
2024-06-13 14:28:02 +0800

    Login Page Gambar

--- src/features/register/pages/RegisterPage.tsx
+++ src/features/register/pages/RegisterPage.tsx
@@ -6,7 +6,7 @@ export const Register = () => {
       <div className="hidden lg:flex items-center h-screen justify-center">
         <img
           loading="lazy"
-          src="/welcome.webp"
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
           alt="image 1"
           className="h-screen object-cover rounded-xl w-full"
         />
